d610c2388b9b1f17cd24846c0751efbc8c36d2d2
Jan Käberich <kaeberic@ibr.cs.tu-bs.de>
2016-02-02 23:27:14 +0100

    added string terminator

--- software/eclipse/electronicLoad/src/string.c
+++ software/eclipse/electronicLoad/src/string.c
@@ -19,17 +19,18 @@
  *                  will be omitted
  */
 void string_fromUint(uint32_t value, char *dest, uint8_t digits, uint8_t dot) {
-	uint32_t divider = 1;
-	uint8_t i;
-	for (i = 1; i < digits; i++)
-		divider *= 10;
+    uint32_t divider = 1;
+    uint8_t i;
+    for (i = 1; i < digits; i++)
+        divider *= 10;
 
-	for (i = digits; i > 0; i--) {
-		if (i == dot)
-			*dest++ = '.';
-		uint8_t c = value / divider;
-		*dest++ = (c % 10) + '0';
-		value -= c * divider;
-		divider /= 10;
-	}
+    for (i = digits; i > 0; i--) {
+        if (i == dot)
+            *dest++ = '.';
+        uint8_t c = value / divider;
+        *dest++ = (c % 10) + '0';
+        value -= c * divider;
+        divider /= 10;
+    }
+    *dest = 0;
 }
