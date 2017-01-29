ed0fabf1cecbe12fc95f41f342005fb417dfb461
Jan Käberich <kaeberic@ibr.cs.tu-bs.de>
2017-01-29 17:40:25 +0100

    label drawing + selectable fixed

--- software/eclipse/electronicLoad/src/GUI/label.c
+++ software/eclipse/electronicLoad/src/GUI/label.c
@@ -8,6 +8,7 @@ void label_create(label_t *l, char *name, font_t font) {
     l->base.func.input = label_input;
     l->font = font;
     l->name = name;
+    l->base.flags.selectable = 0;
     /* calculate size */
     l->base.size.y = fontSize[font].height;
     l->base.size.x = fontSize[font].width * strlen(name);
@@ -15,7 +16,8 @@ void label_create(label_t *l, char *name, font_t font) {
 
 GUIResult_t label_draw(widget_t *w, coords_t offset) {
     label_t *l = (label_t*) w;
-    screen_String(l->name, l->font, offset.x, offset.y);
+    screen_String(l->name, l->font, offset.x + l->base.position.x,
+            offset.y + l->base.position.y);
     return GUI_OK;
 }
 
